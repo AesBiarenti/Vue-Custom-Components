2dffaa424fd3fff458a5fdc9f053ef3ab0de2220
Kadir Sahin <kadirshn2924@gmail.com>
2024-09-10 19:12:09 +0300

    ya allah bismillah

---
+++ vue-components/PrimaryButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="primary">Giriş Yap</button>
+</template>
+<style scoped>
+.primary {
+  background-color: black;
+  border: 0;
+  padding: 8px 20px;
+  border-radius: 5px;
+  color: white;
+  font-weight: 500;
+  transition: 200ms ease;
+  margin: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  min-width: 100px;
+}
+.primary:hover {
+  background-color: rgb(61, 61, 61);
+}
+.primary:active {
+  background-color: rgb(80, 80, 80);
+  transform: scale(0.93);
+}
+</style>
---
+++ vue-components/PrimaryDataTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <table class="primary-table">
+      <thead class="primary-table-head">
+        <th></th>
+      </thead>
+      <tbody class="primary-table-body">
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<style scoped>
+html,
+body {
+  padding: 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.primary-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 25px 0;
+  box-shadow: 0 0 20px rgb(199, 199, 199);
+}
+.primary-table-head {
+  font-size: 1.1em;
+  th {
+    font-weight: 500;
+    background-color: black;
+
+    color: white;
+    padding: 10px;
+  }
+}
+.primary-table-body {
+  font-size: 1em;
+  tr:nth-of-type(even) {
+    background-color: rgb(243, 243, 243);
+  }
+
+  td {
+    padding: 10px 10px;
+    text-align: center;
+    font-weight: 500;
+  }
+}
+</style>
