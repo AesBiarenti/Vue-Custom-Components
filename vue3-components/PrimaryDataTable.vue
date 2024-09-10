7e08c822262d700fb5932f2bb123cfba76fccdd9
Kadir Sahin <kadirshn2924@gmail.com>
2024-09-10 19:43:42 +0300

    09/10/2024

---
+++ vue3-components/PrimaryDataTable.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <table class="primary-table">
+      <thead class="primary-table-head">
+        <tr>
+          <!-- Başlıkları dinamik olarak oluştur -->
+          <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody class="primary-table-body">
+        <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+          <!-- Her satırdaki hücreleri dinamik olarak oluştur -->
+          <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+// Props tanımla: headers ve rows
+const props = defineProps({
+  headers: {
+    type: Array,
+    required: true,
+  },
+  rows: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.primary-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin: 25px 0;
+  box-shadow: 0 0 20px rgb(199, 199, 199);
+}
+
+.primary-table-head {
+  font-size: 1.1em;
+}
+
+.primary-table-head th {
+  font-weight: 500;
+  background-color: black;
+  color: white;
+  padding: 10px;
+}
+
+.primary-table-body {
+  font-size: 1em;
+}
+
+.primary-table-body tr:nth-of-type(even) {
+  background-color: rgb(243, 243, 243);
+}
+
+.primary-table-body td {
+  padding: 10px 10px;
+  text-align: center;
+  font-weight: 500;
+}
+</style>
---
+++ vue3-components/PrimaryInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <input
+    :type="type"
+    :value="modelValue"
+    :placeholder="placeholder"
+    :class="['primary-input', inputStyle]"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text', // Default input type is text, can be changed to email, password, etc.
+  },
+  modelValue: {
+    type: String,
+    default: '', // Two-way binding value
+  },
+  placeholder: {
+    type: String,
+    default: 'Enter value', // Default placeholder, can be customized
+  },
+  inputStyle: {
+    type: String,
+    default: '', // Additional class names can be added here
+  },
+});
+
+const emits = defineEmits(['update:modelValue']);
+</script>
+
+<style scoped>
+.primary-input {
+  border-radius: 5px;
+  border: none;
+  padding: 8px 20px;
+  transition: 200ms;
+  background-color: rgb(243, 243, 243);
+}
+
+.primary-input:hover {
+  background-color: rgb(226, 226, 226);
+}
+
+.primary-input:focus {
+  border: 2px solid rgb(200, 200, 200);
+  outline: none;
+}
+</style>
